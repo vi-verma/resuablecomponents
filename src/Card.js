590b63a32799df9d6775d3a02f92a083ed593d07
Vivek Verma <vivekverma74888@gmail.com>
2021-06-24 19:47:21 +0530

    added logs

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,7 @@
 import classes from './Card.module.css';
 
 function Card(props){
+    console.log("Hwllpksdfn kjds");
     return(
         <div className={`${classes.card} ${props.className}`}>
             {props.children}
